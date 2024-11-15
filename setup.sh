da8a2e2d6e3f1c8e11f8a1bf9dcb348f03fd20f4
qharo <aditya.nair24@gmail.com>
2024-11-15 13:48:55 +0000

    requirements.txt adjusted

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 python3 -m venv .
-source bin/activate
+. /bin/activate
 pip install -r requirements.txt
+pip install torch --index-url https://download.pytorch.org/whl/cpu
 python3 setup.py install
