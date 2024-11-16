bd8cdeb4da6152736575fcdd26e732a72210aeb9
qharo <aditya.nair24@gmail.com>
2024-11-16 10:35:30 +0000

    first chunk

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python3 -m venv .
-. /bin/activate
+. bin/activate
 pip install -r requirements.txt
 pip install torch --index-url https://download.pytorch.org/whl/cpu
 python3 setup.py install
